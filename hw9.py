32cf4750e5e729fb7cdf27693d52c62da6be3f09
JJC <jjc@nlplab.cc>
2018-06-04 11:18:46 +0800

    finish

--- hw9.py
+++ hw9.py
@@ -10,13 +10,56 @@ def init_all_cards():
 
 
 def get_legal_moves(cards_you_have, cards_played, heart_broken=False):
-    # write your code ...
-    return []
+    if cards_played:
+        suit = cards_played[0][0]
+        cards_you_can_play = [card for card in cards_you_have if card.startswith(suit)]
+        if cards_you_can_play:
+            return cards_you_can_play
+        else:
+            return cards_you_have
+    else:
+        if heart_broken:
+            return cards_you_have
+        else:
+            cards_you_can_play = [card for card in cards_you_have if not card.startswith('♥')]
+            if cards_you_can_play:
+                return cards_you_can_play
+            else:
+                return cards_you_have
+
+
+def compute_score(cards):
+    score = sum(1 for card in cards if card.startswith('♥'))
+    if '♠12' in cards:
+        score += 13
+    return score
 
 
 def get_good_moves(cards_you_have, cards_played, heart_broken=False):
-    # write your code ...
-    return []
+    candidates = get_legal_moves(cards_you_have, cards_played, heart_broken)
+    if cards_played:
+        suit = cards_played[0][0]
+        all_numbers = [int(card[1:]) for card in cards_played if card.startswith(suit)]
+        max_number = 1 if 1 in all_numbers else max(all_numbers)
+
+        card_score = {}
+        for card in candidates:
+            card_num = int(card[1:])
+            if card.startswith(suit) and (max_number != 1 and card_num > max_number):
+                score = compute_score(cards_played + [card])
+            else:
+                score = 0
+            card_score[card] = score
+        min_score = min(card_score.values())
+        return [card for card in candidates if card_score[card] == min_score]
+    else:
+        card_score = {}
+        for card in candidates:
+            card_num = int(card[1:])
+            score = compute_score(cards_played + [card])
+            card_score[card] = score
+        min_score = min(card_score.values())
+        return [card for card in candidates if card_score[card] == min_score]
 
 
 def random_cards(cards):
@@ -39,7 +82,7 @@ if __name__ == '__main__':
     for _ in range(10):
         cards_you_have, cards_played, heart_broken = random_cards(cards)
         print("You have:", ', '.join(cards_you_have))
-        print("Cards played:", cards_played)
+        print("Cards played:", cards_played, heart_broken)
 
         cards_you_can_play = get_legal_moves(cards_you_have, cards_played, heart_broken)
         cards_you_should_play = get_good_moves(cards_you_have, cards_played, heart_broken)
