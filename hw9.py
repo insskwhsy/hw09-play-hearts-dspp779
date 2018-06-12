8fa51ed52a5ea5c38368ea8d064176d2b0441e7d
106071004 <37769653+insskwhsy@users.noreply.github.com>
2018-06-12 20:35:19 +0800

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -12,12 +12,13 @@ def init_all_cards():
 def get_legal_moves(cards_you_have, cards_played, heart_broken=False):
     if cards_played:
         suit = cards_played[0][0]
-        card ><
         cards_you_can_play = [card for card in cards_you_have if card.startswith(suit)]
         if cards_you_can_play:
             return cards_you_can_play
         else:
             return cards_you_have
+    elif '♣2' in cards_you_have:
+        return ['♣2']
     else:
         if heart_broken:
             return cards_you_have
@@ -27,42 +28,7 @@ def get_legal_moves(cards_you_have, cards_played, heart_broken=False):
                 return cards_you_can_play
             else:
                 return cards_you_have
-
-
-def compute_score(cards):
-    score = sum(1 for card in cards if card.startswith('♥'))
-    if '♠12' in cards:
-        score += 13
-    return score
-
-
-def get_good_moves(cards_you_have, cards_played, heart_broken=False):
-    candidates = get_legal_moves(cards_you_have, cards_played, heart_broken)
-    if cards_played:
-        suit = cards_played[0][0]
-        all_numbers = [int(card[1:]) for card in cards_played if card.startswith(suit)]
-        max_number = 1 if 1 in all_numbers else max(all_numbers)
-
-        card_score = {}
-        for card in candidates:
-            card_num = int(card[1:])
-            if card.startswith(suit) and ((max_number != 1 and card_num > max_number) or card_num == 1):
-                score = compute_score(cards_played + [card])
-            else:
-                score = 0
-            card_score[card] = score
-        min_score = min(card_score.values())
-        return [card for card in candidates if card_score[card] == min_score]
-    else:
-        card_score = {}
-        for card in candidates:
-            card_num = int(card[1:])
-            score = compute_score(cards_played + [card])
-            card_score[card] = score
-        min_score = min(card_score.values())
-        return [card for card in candidates if card_score[card] == min_score]
-
-
+            
 def random_cards(cards):
     while True:
         cards_you_have = random.sample(cards, random.choice(range(1, 13)))
@@ -77,17 +43,58 @@ def random_cards(cards):
             return cards_you_have, cards_played, heart_broken
 
 
+def get_good_moves(cards_you_have, cards_played, heart_broken=False):
+    _number = {1: 14, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+               10: 10, 11: 11, 12: 12, 13: 13}
+    candidates = get_legal_moves(cards_you_have, cards_played, heart_broken)
+    
+    if cards_played:
+        suit = cards_played[0][0]
+        all_numbers = [int(card[1:]) for card in cards_played if card.startswith(suit)]
+        max_number = 1 if 1 in all_numbers else max(all_numbers)
+        bad_cards = ['♥1','♥2','♥3','♥4','♥5','♥6','♥7','♥8','♥9','♥10','♥11','♥12','♥13','♠12']
+        
+        if any(bad_card in cards_played for bad_card in bad_cards):
+            
+            first_good_cards = [card for card in candidates if card.startswith(suit) and _number[int(card[1:])] < max_number ] + [card for card in candidates if not card.startswith(suit) ]               
+            second_good_cards = [card for card in candidates if card.startswith(suit) and _number[int(card[1:])] > max_number and card != '♠12']
+            third_good_cards = [card for card in candidates if card not in first_good_cards and second_good_cards]
+            if first_good_cards : 
+                return [card for card in first_good_cards]
+            elif second_good_cards:
+                return [card for card in second_good_cards]
+            else:
+                return [card for card in third_good_cards] 
+           
+        else:
+            
+            second_good_cards = [card for card in candidates if card.startswith(suit) and card != '♠12' ] + [card for card in candidates if not card.startswith(suit)]
+            first_good_cards = [card for card in candidates if card.startswith(suit) ] + [card for card in candidates if not card.startswith(suit)]
+            yes_cards = ['♠13','♠1']
+            if yes_cards in cards_played:
+                return [card for card in first_good_cards]
+            else : 
+                return [card for card in second_good_cards]
+    
+            
+            
+    else:
+        return candidates
+        
+        
+        
+
 if __name__ == '__main__':
     cards = init_all_cards()
     cards.remove('♣2')
     for _ in range(10):
         cards_you_have, cards_played, heart_broken = random_cards(cards)
         print("You have:", ', '.join(cards_you_have))
-        print("Cards played:", cards_played, heart_broken)
+        print("Cards played:", cards_played)
 
         cards_you_can_play = get_legal_moves(cards_you_have, cards_played, heart_broken)
         cards_you_should_play = get_good_moves(cards_you_have, cards_played, heart_broken)
         print("You can play:", ', '.join(cards_you_can_play))
-        print("You should play:", ', '.join(cards_you_should_play))
+        print("You should play:", ',' .join(cards_you_should_play))
 
         print()
